d94ceaf01261900263b0db73d304e64ba2b649d6
BinhPham <binhpham@b13technology.com>
2022-09-18 00:22:39 +0700

    fix: fix bug update user

--- src/user/dto/user.update-code.dto.ts
+++ src/user/dto/user.update-code.dto.ts
@@ -3,43 +3,42 @@ import {
     IsString,
     IsNotEmpty,
     MaxLength,
-    IsOptional, IsMongoId,
+    IsOptional, IsMongoId, IsArray,
 } from 'class-validator';
 
 export class UserUpdateCodeDto {
     @IsNotEmpty()
     @IsString()
-    @MaxLength(30)
+    @MaxLength(100)
     @Type(() => String)
     readonly codeEmployee: string;
 
     @IsString()
     @IsOptional()
-    @MaxLength(30)
+    @MaxLength(100)
     @Type(() => String)
     readonly codeAM?: string;
 
     @IsString()
     @IsOptional()
-    @MaxLength(30)
+    @MaxLength(100)
     @Type(() => String)
     readonly codeDepartmentLevelSix?: string;
 
-    @IsString()
+    @IsArray()
     @IsOptional()
-    @MaxLength(30)
     @Type(() => Array)
     readonly codeLevelSix?: string[];
 
     @IsString()
     @IsOptional()
-    @MaxLength(30)
+    @MaxLength(100)
     @Type(() => String)
     readonly position?: string;
 
     @IsOptional()
     @IsMongoId()
-    @MaxLength(30)
+    @MaxLength(100)
     @Type(() => String)
     readonly role?: string;
 }
